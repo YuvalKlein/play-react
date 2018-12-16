d5609557047ea02c81caaf1097f1885521d3164b
yuval@yk-group.org <yuval@yk-group.org>
2018-12-16 14:12:54 +0200

    add redux

--- src/components/SessionList/SessionList.js
+++ src/components/SessionList/SessionList.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 // import './SessionList.css';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import { Button } from 'reactstrap';
 
+import * as mainActions from "./../../actions/mainAction";
 import SessionView from './SessionView/SessionView';
+import NewSession from './NewSession/NewSession';
 let mockData = require('./mockData')
 
 class SessionList extends Component {
-
+ 
+  componentWillMount(){
+    this.props.createSessionList(mockData.sessionList);
+  }
   render() {
-    console.log(mockData.sessionList)
-    const sessions = mockData.sessionList.map((session, index) => {
+    let sessionList = this.props.sessionList
+    if(sessionList){
+      const sessions = sessionList.map((session, index) => {
+        return (
+          <SessionView key={index} session={session}/>
+        );
+      });
+  
       return (
-        <SessionView key={index} session={session}/>
+        <div >
+            <h1>SessionList</h1>
+          {sessions}
+          <NewSession/>
+        </div>
       );
-    });
+    }else{
+      return  <h2>Loading...</h2>  
+    }
 
-    return (
-      <div >
-          <h1>SessionList</h1>
-        {sessions}
-        <button>+</button>
-      </div>
-    );
   }
 }
+const mapStateToProps = state => ({
+  name: state.mainReducer.name,
+  sessionList: state.mainReducer.sessionList,
 
-export default SessionList;
+});
+function mapDispatchToProps(dispatch) {
+  return {...bindActionCreators(mainActions, dispatch)}
+};
+export default connect(mapStateToProps,mapDispatchToProps)(SessionList);
--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
+import store from './reducer/index'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>   , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>  
+     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
