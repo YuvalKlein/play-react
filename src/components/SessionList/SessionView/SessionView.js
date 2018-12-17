f76a034c60e806fd222e3361a875dba131e6e64a
yuval@yk-group.org <yuval@yk-group.org>
2018-12-17 17:42:51 +0200

    style session view

--- src/components/SessionList/SessionView/SessionView.js
+++ src/components/SessionList/SessionView/SessionView.js
@@ -4,13 +4,14 @@ import classes from './SessionView.css';
 
 const sessionView = (props) => 
       <div  className={classes.SessionView}>
-        <div className={classes.Time} >{props.session.time}</div>
-        <div className={classes.Content}>
-          <div className={classes.Info} >
-            <p className={classes.SessionName}>{props.session.sessionName}</p>
+        <div className={classes.Time} >
+          <div>{props.session.time}</div>
+          <div>{props.session.endTime}</div>
+        </div>
+        <div className={classes.Info}>
+            <p className={classes.Title}>{props.session.title}</p>
             <p>{props.session.location}</p>
-          </div>
-          <div className={classes.Players}>{props.session.players.map((player,i) => <div key={i}>{player.avatar}</div>)}</div>
+          {/* <div className={classes.Players}>{props.session.players.map((player,i) => <div key={i}>{player.avatar}</div>)}</div> */}
         </div>
         <div className={classes.Button}>
           <button >BOOK</button>
