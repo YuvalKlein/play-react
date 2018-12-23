1bdcf1a57cab622593d6c9590278af4d28f7ca72
yuval@yk-group.org <yuval@yk-group.org>
2018-12-23 13:57:28 +0200

    build

---
+++ build/precache-manifest.43235ed8617863e36f2d3dbe08fa51f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "39bc7bcbf36a808bfa95ef3533330386",
+    "url": "/static/media/logo.39bc7bcb.bmp"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f0704dc9f5175122af6",
+    "url": "/static/js/main.6f0704dc.chunk.js"
+  },
+  {
+    "revision": "6f385064ddb53cd0f285",
+    "url": "/static/js/1.6f385064.chunk.js"
+  },
+  {
+    "revision": "6f0704dc9f5175122af6",
+    "url": "/static/css/main.a7fb0371.chunk.css"
+  },
+  {
+    "revision": "6f385064ddb53cd0f285",
+    "url": "/static/css/1.d48a6e3e.chunk.css"
+  },
+  {
+    "revision": "25e07bc03a9640e9009fb514a62d5f68",
+    "url": "/index.html"
+  }
+];
